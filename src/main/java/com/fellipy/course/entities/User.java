c8924a79833ddf3d75bf918df43e108d621164db
fellipySaldanha <fellipy.saldanha@gmail.com>
2019-11-14 17:19:18 -0300

    database, test profile, JPA

--- src/main/java/com/fellipy/course/entities/User.java
+++ src/main/java/com/fellipy/course/entities/User.java
@@ -1,12 +1,19 @@
 package com.fellipy.course.entities;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class User implements Serializable{
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private static final long serialVersionUID = 1L;
 	private String name;
-	private Long id;
 	private String email;
 	private String phone;
 	private String password;
