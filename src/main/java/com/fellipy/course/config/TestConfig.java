db84eababd5a7760e68696e5c7a78c0112c937ef
fellipySaldanha <fellipy.saldanha@gmail.com>
2019-11-19 19:08:18 -0200

    Payment, one-to-one association

--- src/main/java/com/fellipy/course/config/TestConfig.java
+++ src/main/java/com/fellipy/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.fellipy.course.entities.Category;
 import com.fellipy.course.entities.Order;
 import com.fellipy.course.entities.OrderItem;
+import com.fellipy.course.entities.Payment;
 import com.fellipy.course.entities.Product;
 import com.fellipy.course.entities.User;
 import com.fellipy.course.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment payment = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(payment);
+		
+		orderRepository.save(o1);
 		
 	}	
 	
