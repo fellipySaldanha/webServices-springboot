adb7bcc9185adecfa7b28bb780711fe4b77f859e
fellipySaldanha <fellipy.saldanha@gmail.com>
2019-12-01 20:39:58 -0300

    delete exception

--- src/main/java/com/fellipy/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/fellipy/course/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.fellipy.course.services.exceptions.DatabaseException;
 import com.fellipy.course.services.exceptions.ResourceNotFoundException;
 
 import org.springframework.http.HttpStatus;
@@ -20,6 +21,14 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException error, HttpServletRequest request){
         String errorMsg = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError standardError = new StandardError(Instant.now(), status.value(), errorMsg, error.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(standardError);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException error, HttpServletRequest request){
+        String errorMsg = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError standardError = new StandardError(Instant.now(), status.value(), errorMsg, error.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(standardError);
     }
