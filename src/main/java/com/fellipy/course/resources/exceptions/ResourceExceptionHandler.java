0b1d5c2afeff91b4d926b76b45510ce364b4804d
fellipySaldanha <fellipy.saldanha@gmail.com>
2019-11-30 23:42:35 -0300

    Exceptions

---
+++ src/main/java/com/fellipy/course/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.fellipy.course.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.fellipy.course.services.exceptions.ResourceNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+
+@ControllerAdvice 
+public class ResourceExceptionHandler {
+    
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException error, HttpServletRequest request){
+        String errorMsg = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError standardError = new StandardError(Instant.now(), status.value(), errorMsg, error.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(standardError);
+    }
+    
+}
--- src/main/java/com/fellipy/course/services/UserService.java
+++ src/main/java/com/fellipy/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.fellipy.course.entities.User;
 import com.fellipy.course.repositories.UserRepository;
+import com.fellipy.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 		
 	public User findById(Long id) {
 		Optional<User> user = userRepository.findById(id);
-		return user.get();
+		return user.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User user){
